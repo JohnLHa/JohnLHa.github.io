c2ce1b9d105fa993514c2f53fbde81a590de9783
CVCF Dev Team <cvcfdevteam@gmail.com>
2019-02-01 14:12:00 -0500

    Allow discount for any type of discount or testing

--- polyfills.js
+++ polyfills.js
@@ -5496,8 +5496,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\John Ha\Documents\Camps\CVCF\Code\registration\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\John Ha\Documents\Camps\CVCF\Code\registration\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\jha\Documents\CVCF\registration\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\jha\Documents\CVCF\registration\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
